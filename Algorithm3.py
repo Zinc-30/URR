209c399cfe098f7834bd8427ea24a3c8b47a17a5
xinhao <xinhao20122013@hotmail.com>
2017-02-21 22:53:20 +0800

    debug on cid,qid

--- Algorithm3.py
+++ Algorithm3.py
@@ -1,45 +1,59 @@
-from Algorithm1 import Algo1 
+from Algorithm1 import Algo1
 import readData as rd
 
-def efficiencyGreedy(cost,cars,quests,utility,S,sim,paras):
-	pairSet = []
-	a1 = Algo1(cost)
-	for qi in range(len(quests)):
-		for ci in range(len(cars)):
-			tmpS = a1.ScheduleSingleRequest(S[ci],cars[ci],quests[qi],qi);
-			if tmpS:
-				cost1 = rd.calCost(S[ci],cost);
-				cost2 = rd.calCost(tmpS,cost);
-				u0 = rd.cal_u_car(ci,S[ci],utility,sim,cost,quests,paras)
-				u1 = rd.cal_u_car(ci,tmpS,utility,sim,cost,quests,paras)
-				pairSet.append([qi,ci,(u1-u0)*1.0/(cost2-cost1+0.001)])
-	
-	pairSet = sorted(pairSet,key= lambda x:x[2],reverse=True)
-	while pairSet:
-		# print pairSet
-		qi = pairSet[0][0]
-		ci = pairSet[0][1]
-		S[ci] = a1.ScheduleSingleRequest(S[ci],cars[ci],quests[qi],qi);
-		#print S[ci]
-                del pairSet[0]
-		# print "qi,ci",qi,ci 
-		for i in range(len(pairSet)-1,-1,-1):
-			if pairSet[i][0]==qi:
-				del pairSet[i]
-				continue
-			if pairSet[i][1]==ci:
-				tmpS = a1.ScheduleSingleRequest(S[ci],cars[ci],quests[pairSet[i][0]],pairSet[i][0])
-				if tmpS:
-					cost1 = rd.calCost(S[ci],cost)
-					cost2 = rd.calCost(tmpS,cost)
-					u0 = rd.cal_u_car(ci,S[ci],utility,sim,cost,quests,paras)
-					u1 = rd.cal_u_car(ci,tmpS,utility,sim,cost,quests,paras)
-					pairSet[i][2] = (u1-u0)*1.0/(cost2-cost1+0.01)
-				else:
-					del pairSet[i]
-		pairSet = sorted(pairSet,key= lambda x:x[2],reverse=True)
-	# print S
-	return S
+
+def efficiencyGreedy(cost, cars, quests, utility, S, sim, paras, c2id=None, q2id=None):
+    pairSet = []
+    a1 = Algo1(cost)
+    for qi in range(len(quests)):
+        for ci in range(len(cars)):
+            if c2id:
+                cid = c2id[ci]
+            else:
+                cid = ci
+            if q2id:
+                qid = q2id[qi]
+            else:
+                qid = qi
+            tmpS = a1.ScheduleSingleRequest(
+                S[cid], cars[cid], quests[qid], qid)
+            if tmpS:
+                cost1 = rd.calCost(S[cid], cost)
+                cost2 = rd.calCost(tmpS, cost)
+                u0 = rd.cal_u_car(cid, S[cid], utility,
+                                  sim, cost, quests, paras)
+                u1 = rd.cal_u_car(cid, tmpS, utility, sim, cost, quests, paras)
+                pairSet.append(
+                    [qid, cid, (u1 - u0) * 1.0 / (cost2 - cost1 + 0.001)])
+    pairSet = sorted(pairSet, key=lambda x: x[2], reverse=True)
+    while pairSet:
+        # print pairSet
+        qi = pairSet[0][0]
+        ci = pairSet[0][1]
+        S[ci] = a1.ScheduleSingleRequest(S[ci], cars[ci], quests[qi], qi)
+        # print S[ci]
+        del pairSet[0]
+        # print "qi,ci",qi,ci
+        for i in range(len(pairSet) - 1, -1, -1):
+            if pairSet[i][0] == qi:
+                del pairSet[i]
+                continue
+            if pairSet[i][1] == ci:
+                tmpS = a1.ScheduleSingleRequest(
+                    S[ci], cars[ci], quests[pairSet[i][0]], pairSet[i][0])
+                if tmpS:
+                    cost1 = rd.calCost(S[ci], cost)
+                    cost2 = rd.calCost(tmpS, cost)
+                    u0 = rd.cal_u_car(
+                        ci, S[ci], utility, sim, cost, quests, paras)
+                    u1 = rd.cal_u_car(ci, tmpS, utility, sim,
+                                      cost, quests, paras)
+                    pairSet[i][2] = (u1 - u0) * 1.0 / (cost2 - cost1 + 0.01)
+                else:
+                    del pairSet[i]
+        pairSet = sorted(pairSet, key=lambda x: x[2], reverse=True)
+    # print S
+    return S
 
 # def efficiencyGreedy_old(cost,cars,quests,utility,S):
 # 	pairSet = []
@@ -51,7 +65,7 @@ def efficiencyGreedy(cost,cars,quests,utility,S,sim,paras):
 # 				cost1 = calCost(S[ci],cost);
 # 				cost2 = calCost(tmpS,cost);
 # 				pairSet.append([qi,ci,utility[qi][ci]*1.0/(cost2-cost1+0.001)])
-	
+
 # 	pairSet = sorted(pairSet,key= lambda x:x[2],reverse=True)
 # 	while pairSet:
 # 		# print len(pairSet)
@@ -60,7 +74,7 @@ def efficiencyGreedy(cost,cars,quests,utility,S,sim,paras):
 # 		ci = pairSet[0][1]
 # 		S[ci] = a1.ScheduleSingleRequest(S[ci],cars[ci],quests[qi],qi);
 # 		del pairSet[0]
-# 		# print "qi,ci",qi,ci 
+# 		# print "qi,ci",qi,ci
 # 		for x in pairSet:
 # 			if x[0]==qi:
 # 				del x
--- Algorithm6.py
+++ Algorithm6.py
@@ -3,33 +3,39 @@ import readData as rd
 
 def purnCars(quests,center,cars,areadict,radius,cost):
 	carc = []
-	maxt = 0;
+	carids = []
+	maxt = 0
 	for q in quests:
 		if q[1] >maxt:
 			maxt = q[1]
-	for car in cars:
+	for cid in range(len(cars)):
+		car = cars[cid]
 		if center in radius and rd.getCost(car[0],center,cost)<maxt+radius[center]:
 			carc.append(car)
- 	return carc
+			carids.append(cid)
+ 	return carc,carids
 
 def groupScheduling(cost,cars,quests,utility,S,areadict,radius,sim,paras):
 	g={}
+	q2id_g = {}
 	grest = []
-	for q in quests:
+	q2id_rest = []
+	for qi in range(len(quests)):
+		q = quests[qi]
 		if q[0] in areadict and q[3] in areadict and areadict[q[0]] == areadict[q[3]]:
 			ax = areadict[q[0]]
 			if ax not in g:
 				g[ax] = []
 			g[ax].append(q)
+			q2id_g[ax].append(qi)
 		else:
 			grest.append(q)
-	gnew = [[g[t],areadict[g[t][0][0]]] for t in sorted(g,key = lambda x:len(g[x]),reverse=1)]
-	print "grest",len(grest)
-	print "gnew",len(quests)-len(grest)
-	S = Algorithm3.efficiencyGreedy(cost,cars,grest,utility,S,sim,paras)
-	for gx in gnew:
-		carc = purnCars(gx[0],gx[1],cars,areadict,radius,cost)
-		S = Algorithm3.efficiencyGreedy(cost,carc,gx[0],utility,S,sim,paras)
+			q2id_rest.append(qi)
+	gnew = sorted(g,key = lambda x:len(g[x]),reverse=1)
+	S = Algorithm3.efficiencyGreedy(cost,cars,grest,utility,S,sim,paras,q2id = q2id_rest)
+	for c in gnew:
+		carc, carcid = purnCars(g[c],c,cars,areadict,radius,cost)
+		S = Algorithm3.efficiencyGreedy(cost,carc,g[c],utility,S,sim,paras,c2id = carcid,q2id = q2id_g[c])
 	return S
 
 
