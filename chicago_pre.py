cda2e792877eb5a7965ad3a8cae8bd11a3e388dd
xinhao <xinhao20122013@hotmail.com>
2017-02-22 17:56:16 +0800

    add chicago test

---
+++ chicago_pre.py
@@ -0,0 +1,145 @@
+import networkx as nx
+import numpy as np
+import random
+import pandas as pd
+import pp
+import os
+import pickle
+import readData as rd
+def calcost(G,subl,wl):
+	cost = dict()
+	for s in subl:
+		cost[s] = {}
+		for t in wl:
+			cost[s][t] = nx.astar_path_length(G,source=s,target=t,weight='weight')
+	return cost
+def sNodes(filename):
+	nodes = []
+	nodeid = []
+	for line in open('chicago/chicago_points.csv','r'):
+		tmps = line[:-1].split(',')
+		nodeid.append(int(tmps[0]))
+		nodes.append([int(tmps[1]),int(tmps[2])])
+	return nodeid,nodes
+
+def get_nodes_trip(limit,filename,nodes,nodeid):
+	select_df = pd.read_csv('chicago/trip.csv')
+	print select_df.info()
+	return set(select_df['x']) | set(select_df['y'])
+
+def sRoad(limit,filename,nodes,ncores):
+	# cost_file = 'data/old_cost.npy'
+	# calc =======================
+	if not os.path.exists('chicago/cost.npy'):
+		G = nx.Graph()
+		for line in open(filename,'r'):
+			tmps = line[:-1].split(',')
+			G.add_edge(int(tmps[0]), int(tmps[1]), weight = int(tmps[2]))
+		print "generate into G.node num",len(G.nodes())
+		print "generate into G.edges num",len(G.edges())
+		pickle.dump(G, open('chicago/graph.txt', 'w'))
+		nodelist = list(nodes)
+		nodelist.sort()
+		cost = dict()
+		print "calc cost distance between nodes",len(nodelist)
+			# ++++=pp-version =========
+		if ncores>1:
+			jobs = []
+		 	job_server = pp.Server(ncores)
+		 	inter = len(nodelist)/ncores
+		 	for ni in range(ncores):
+		 		subl = nodelist[ni*inter:(ni+1)*inter]
+		 		jobs.append(job_server.submit(calcost,(G,subl,nodelist),(),("import networkx as nx",)))
+		 	jobs.append(job_server.submit(calcost,(G,nodelist[ncores*inter:],nodelist),(),("import networkx as nx",)))
+		 	job_server.wait()
+		 	for job in jobs:
+		 		job_ans = job()
+		 		cost.update(job_ans)
+		else:
+			# ++++=1s-version ========
+			for i in range(len(nodelist)):
+				s = nodelist[i]
+				if i%10==0:
+					print 'doing',s,i,'/',len(nodelist)
+				if s not in cost:
+					cost[s] = {}
+				for j in range(len(nodelist)):
+					t = nodelist[j]
+					if t not in cost[s]:
+						cost[s][t] = nx.astar_path_length(G,source=s,target=t,weight='weight')
+						if t not in cost:
+							cost[t] = {}
+						cost[t][s] = cost[s][t]
+			#===save data==========
+		# np.save('chicago/'+str(limit)+'_nodes.npy', np.array(nodelist))
+		np.save('chicago/cost.npy', np.array(cost))
+		print "finish save data"
+	# read ==========================
+	else:
+		print 'read cost'
+		cost = np.load('chicago/cost.npy').tolist()
+	return cost
+
+def sCars(num,room,nodes):
+	cars = []
+	for i in range(num):
+		lid = random.sample(nodes,1)[0]
+		cars.append([lid,room])
+	np.save('chicago/'+str(num)+'.'+str(room)+'-cars.npy',np.array(cars))
+	return cars
+def sQuest(count,pt,eps,cost):
+	# =================================
+	df = pd.read_csv('chicago/'+str(count)+'K_trips.csv')
+	quest = []
+	# =================================
+	for rid in range(len(df)):
+		bnode = int(df.iloc[rid][0])
+		enode = int(df.iloc[rid][1])
+		timec = rd.getCost(bnode,enode,cost)
+		btime = random.randint(pt[0]*300,pt[1]*300)
+		etime = btime + timec * eps
+		# print bnode,enode,timec,cost[bnode][enode]
+		quest.append([bnode,btime,etime,enode])
+	np.save('chicago/'+str(count)+'.'+str(pt[0])+'.'+str(eps)+'-quests.npy',np.array(quest))
+
+def pre_main_data():
+	limit = 1000
+	node_file = 'chicago/chicago_points.csv'
+	graph_file = 'chicago/chicago_roads.csv'
+	trip_file = 'chicago/2K_trips_chicago.csv'
+	# request_file = 'data/50_select_nodes.csv'
+	nodesid,nodes= sNodes(node_file)
+	# print nodes
+	nodes_set = get_nodes_trip(limit,trip_file,nodes,nodesid)
+	print "slect node num",len(nodes_set)
+	#=======================================================
+	cost = sRoad(limit,graph_file,nodes_set,1)
+	#======================================================
+	print 'doing riders'
+	for count in [1,3]:
+		for pt in [[1,10],[10,30],[30,60]]:
+			sQuest(count,pt,1.5,cost)
+	for eps in [1.2,1.7,2.0]:
+		sQuest(3,[10,30],eps,cost)
+
+	print 'doing cars'
+	for count in [100,200,300,400,500]:
+		sCars(count,3,nodes_set)
+	for room in [2,4,5]:
+		sCars(200,room,nodes_set)
+	print 'doing utility'
+	for ncars in [100,200,300,400,500]:
+		for nquest in [1,3]:
+			utility = [[random.random() for i in range(ncars)] for j in range(nquest*1000)]
+			np.save('chicago/'+str(nquest)+'.'+str(ncars)+'-utility.npy',np.array(utility))
+			
+	print 'doing similarity'
+	for nquest in [1,3]:
+		for nquest in [1,3,5,8,10]:
+			sim = [[random.random() for i in range(nquest*1000)] for j in range(nquest*1000)]
+			np.save('chicago/'+str(nquest)+'.'+str(nquest)+'-sim.npy',np.array(sim))
+
+
+if __name__ == '__main__':
+	pre_main_data()
+	# pre_toy_data()
---
+++ chicago_test.py
@@ -0,0 +1,171 @@
+import readData as rd
+import pandas as pd
+import numpy as np
+import pickle
+import Algorithm2 as A2
+import Algorithm3 as A3
+import Algorithm6 as A6
+import Algorithm7 as A7
+import baseline_cost
+import baseline_utility
+from time import clock
+import os
+import pp
+
+def task(method,cost,quests,cars,utility,sim,paras,k):
+	def getDict(k):
+		filename = 'chicago/areadict-'+str(k)+'.npy'
+		if os.path.exists(filename):
+			print 'read dict data'
+			return pickle.load(open(filename, 'r'))
+		else:
+			print 'no dict data'
+	def getRadius(k):
+		filename = 'chicago/areaR-'+str(k)+'.npy'
+		if os.path.exists(filename):
+			print 'read radic data'
+			return pickle.load(open(filename, 'r'))
+		else:
+			print 'no dict data'
+	S = [[] for i in range(len(cars))]
+	print "begin"
+	start = clock()
+	if method == 0:
+		ans = A2.bilateralArrangement(cost,cars,quests,utility,S,sim,paras)
+	elif method ==1:
+		ans = A3.efficiencyGreedy(cost,cars,quests,utility,S,sim,paras)
+	elif method ==2:
+		areadict = getDict(k)
+		radius = getRadius(k)
+		ans = A6.groupScheduling(cost,cars,quests,utility,S,areadict,radius,sim,paras)
+	elif method ==3:
+		areadict = getDict(k)
+		radius = getRadius(k)
+		ans = A7.groupScheduling(cost,cars,quests,utility,S,areadict,radius,sim,paras)
+	elif method ==4:
+		ans = baseline_cost.costGreedy(cost,cars,quests,utility,S,sim,paras)
+	elif method ==5:
+		ans = baseline_utility.utilityGreedy(cost,cars,quests,utility,S,sim,paras)
+	end = clock()
+	time = end-start
+	sum_u = rd.cal_u_all(ans,utility,sim,cost,quests,paras)
+	print "=========method",method
+	#print "answer is ",ans
+	print "time:", time
+	print "sum utility", sum_u
+	return ans,time,sum_u
+
+def test(testname,cost,quests,cars,utility,sim,k,paras):
+	# print "==============",numq,numc,pt,roomc,"============" 
+	res = []
+	for i in range(6):
+		if i!=7:
+			ans,time,sumu = task(i,cost,quests,cars,utility,sim,paras,k)
+			Riders = set()
+			for s in ans:
+				for x in s:
+					Riders = Riders|x['riders']
+			sum_r = len(Riders)
+		else:
+			sumu = 0
+			time = 0
+			sum_r = 0
+		res.append(time)
+		res.append(sumu)
+		res.append(sum_r)
+	s = pd.Series(res,index = ['2-time','2-u','2-r','3-time','3-u','3-r','2+group-time','2+group-u','2+group-r',\
+		'3+group-time','3+group-u','3+group-r','gt-time','gt-u','gt-r','gu-time','gu-u','gu-r'])
+	s.to_csv('chicago/'+testname+'.csv')
+	return testname,s
+
+def test_main():
+	result = pd.DataFrame([])
+	cost = rd.readCost('chicago/cost.npy')
+	ncores = 20
+	jobs = []
+	job_server = pp.Server(ncores)
+
+	# test defult
+	cars = rd.readCars(200,3)
+	quests = rd.readQuests(3,1,1.5)
+	utility = rd.readUtility(3,200)
+	sim = rd.readSim(3,3)
+	k = 10
+	paras = [0.33,0.33]
+
+	jobs.append(job_server.submit(test,('test-default',cost,quests,cars,utility,sim,k,paras),(task,),\
+		('import os','from time import clock','import numpy as np','import pandas as pd','import readData as rd',\
+			'import baseline_cost','import baseline_utility','import Algorithm2 as A2','import Algorithm3 as A3','import Algorithm6 as A6','import Algorithm7 as A7')))
+	
+	# test paras
+	for paras in [[1,0],[0,0],[0,1]]:
+		jobs.append(job_server.submit(test,('test-para='+str(paras),cost,quests,cars,utility,sim,k,paras),(task,),\
+		('import os','from time import clock','import numpy as np','import pandas as pd','import readData as rd',\
+			'import baseline_cost','import baseline_utility','import Algorithm2 as A2','import Algorithm3 as A3','import Algorithm6 as A6','import Algorithm7 as A7')))
+	paras = [0.33,0.33]
+
+	# test quest num
+	for count in [1]:
+		quests = rd.readQuests(count,1,1.5)
+		utility = rd.readUtility(count,200)
+		sim = rd.readSim(count,count)
+		jobs.append(job_server.submit(test,('test-rider_num='+str(count),cost,quests,cars,utility,sim,k,paras),(task,),\
+		('import os','from time import clock','import numpy as np','import pandas as pd','import readData as rd',\
+			'import baseline_cost','import baseline_utility','import Algorithm2 as A2','import Algorithm3 as A3','import Algorithm6 as A6','import Algorithm7 as A7')))
+	quests = rd.readQuests(3,1,1.5)
+	utility = rd.readUtility(3,200)
+	sim = rd.readSim(3,3)
+
+	# test quest pt
+	for pt in [[10,30],[30,60]]:
+		quests = rd.readQuests(3,pt[0],1.5)
+		jobs.append(job_server.submit(test,('test-rider_pt='+str(pt),cost,quests,cars,utility,sim,k,paras),(task,),\
+		('import os','from time import clock','import numpy as np','import pandas as pd','import readData as rd',\
+			'import baseline_cost','import baseline_utility','import Algorithm2 as A2','import Algorithm3 as A3','import Algorithm6 as A6','import Algorithm7 as A7')))
+
+	# test quest eps
+	for eps in [1.2,1.7,2.0]:
+		quests = rd.readQuests(3,1,eps)
+		jobs.append(job_server.submit(test,('test-rider_eps='+str(eps),cost,quests,cars,utility,sim,k,paras),(task,),\
+		('import os','from time import clock','import numpy as np','import pandas as pd','import readData as rd',\
+			'import baseline_cost','import baseline_utility','import Algorithm2 as A2','import Algorithm3 as A3','import Algorithm6 as A6','import Algorithm7 as A7')))
+
+
+	# test car num
+	for count in [100,300,400,500]:
+		cars = rd.readCars(count,3)
+		utility = rd.readUtility(3,count)
+		jobs.append(job_server.submit(test,('test-car_num='+str(count),cost,quests,cars,utility,sim,k,paras),(task,),\
+		('import os','from time import clock','import numpy as np','import pandas as pd','import readData as rd',\
+			'import baseline_cost','import baseline_utility','import Algorithm2 as A2','import Algorithm3 as A3','import Algorithm6 as A6','import Algorithm7 as A7')))
+	cars = rd.readCars(200,3)
+	utility = rd.readUtility(3,200)	
+
+	# test car room
+	for room in [2,4,5]:
+		cars = rd.readCars(200,room)
+		jobs.append(job_server.submit(test,('test-car_room='+str(room),cost,quests,cars,utility,sim,k,paras),(task,),\
+		('import os','from time import clock','import numpy as np','import pandas as pd','import readData as rd',\
+			'import baseline_cost','import baseline_utility','import Algorithm2 as A2','import Algorithm3 as A3','import Algorithm6 as A6','import Algorithm7 as A7')))
+
+	for job in jobs:
+		job_name,job_ans = job()
+		result[job_name] = job_ans
+	result.to_csv('chicago/result.csv')
+
+def test_toy():
+	cost = np.load('toy_cost.npy').tolist()
+	utility = np.load('toy_utility.npy').tolist()
+	cars = np.load('toy_cars.npy').tolist()
+	quests = np.load('toy_quests.npy').tolist()
+	sim = np.load('toy_sim.npy').tolist()
+	nquest = len(quests)
+	ncars = len(cars)
+	k = 3
+	print nquest,ncars
+	print test('toy',cost,quests,cars,utility,sim,k,[0.3,0.3])
+	print 'best',1.86218116608
+
+if __name__ == "__main__":
+	# test_toy()
+	test_main()
