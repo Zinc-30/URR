9ceb966d07c1c8d12658523ce5aef4a6c536799c
xinhao <xinhao20122013@hotmail.com>
2017-02-22 16:49:11 +0800

    test

--- test.py
+++ test.py
@@ -91,7 +91,9 @@ def test_main():
 	utility = rd.readUtility(3,200)
 	sim = rd.readSim(3,3)
 	k = 10
-	paras = [0.33,0.33]
+	# paras = [0.33,0.33]
+	paras = [0,1]
+
 
 	# jobs.append(job_server.submit(test,('test-default',cost,quests,cars,utility,sim,k,paras),(task,),\
 	# 	('import os','from time import clock','import numpy as np','import pandas as pd','import readData as rd',\
